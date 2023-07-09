5a441b842ba6f25213e4e6b8c69663d5d447fa4b
james-mumo <mumojames98@gmail.com>
2023-07-09 09:07:52 +0300

    fixed the header

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
+
 app.get('/api/keys/google', (req, res) => {
   res.send({ key: process.env.GOOGLE_API_KEY || '' });
 });
