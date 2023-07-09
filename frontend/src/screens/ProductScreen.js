18888bcd8da2d46698d508d7e3f1d83145f5a940
james-mumo <mumojames98@gmail.com>
2023-07-09 06:28:24 +0300

    fixed the slug issuesr

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -59,7 +59,9 @@ function ProductScreen() {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get(`/api/products/slug/${slug}`);
+        const result = await axios.get(
+          `${process.env.REACT_APP_BACKEND}/api/products/slug/${slug}`
+        );
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
@@ -73,7 +75,9 @@ function ProductScreen() {
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`${process.env.REACT_APP_BACKEND}/api/products/${product._id}`);
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_BACKEND}/api/products/${product._id}`
+    );
     if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
